18b70e6aa87a49b9f0b70db58fe6f10d3925b4bf
Gokul-Js <sharma.gokul258@gmail.com>
2021-12-02 01:01:28 +0530

    ORDER_DETAIL_COMMIT

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -8,7 +8,11 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
-  ORDER_PAY_RESET
+  ORDER_PAY_RESET,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_RESET
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -57,27 +61,53 @@ export const orderDetailsReducer = (state = { loading: true, orderItems: [], shi
 }
 
 export const orderPayReducer = (
-  state = {  }, 
-  action
-) => {
-    switch(action.type) {
-      case ORDER_PAY_REQUEST:
-        return {
-          loading: true,
-        }
-      case ORDER_PAY_SUCCESS:
-        return {
-          loading: false,
-          success: true,
-        }
-      case ORDER_PAY_FAIL:
-        return {
-          loading: false,
-          error: action.payload,
-        }
-      case ORDER_PAY_RESET:
-        return {}
-      default:
-        return state
+    state = {  }, 
+    action
+  ) => {
+      switch(action.type) {
+        case ORDER_PAY_REQUEST:
+          return {
+            loading: true,
+          }
+        case ORDER_PAY_SUCCESS:
+          return {
+            loading: false,
+            success: true,
+          }
+        case ORDER_PAY_FAIL:
+          return {
+            loading: false,
+            error: action.payload,
+          }
+        case ORDER_PAY_RESET:
+          return {}
+        default:
+          return state
+      }
     }
-  }
+
+  export const orderListMyReducer = (
+   state = { orders:[] }, 
+      action
+  ) => {
+        switch(action.type) {
+          case ORDER_LIST_MY_REQUEST:
+            return {
+              loading: true,
+            }
+          case ORDER_LIST_MY_SUCCESS:
+            return {
+              loading: false,
+              orders: action.payload,
+            }
+          case ORDER_LIST_MY_FAIL:
+            return {
+              loading: false,
+              error: action.payload,
+            }
+          case ORDER_LIST_MY_RESET:
+            return { orders: []}
+          default:
+            return state
+        }
+      }
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -38,7 +38,7 @@ const OrderScreen = ({ match }) => {
       const { data: clientId } = await axios.get('/api/config/paypal')
       const script = document.createElement('script')
       script.type = 'text/javascript'
-      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=INR`
       script.async = true
       script.onload = () => {
         setSdkReady(true)
@@ -174,6 +174,7 @@ const successPaymentHandler = (paymentResult) => {
                             <Loader /> 
                           ) : (
                             <PayPalButton 
+                              currency="INR"
                               amount={order.totalPrice}
                               onSuccess={successPaymentHandler}
                             />
